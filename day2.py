86bbc96f76a33db1526b7597f6a2f178f3dd1533
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2020-12-07 23:33:48 -0800

    completed day2

--- day2.py
+++ day2.py
@@ -5,22 +5,33 @@ def get_passwords(filename):
     passwords = [pw for pw in file.readlines()]
     return passwords
 
-def check_rule(password_rule):
+def password_parse(password_rule):
     split = password_rule.split(" ")
     nums = [int(e) for e in split[0].split("-")]
-    char_rule = split[1].split(":")[0]
-    char_value = 0
-    for char in split[2]:
-        char_value += char == char_rule
-        if char_value > nums[1]:
-            return False
-    return char_value >= nums[0]
+    char = split[1].split(":")[0]
+    return((int(nums[0]), int(nums[1]), char, split[2]))
 
-def check_passwords(passwords):
-    return sum([check_rule(pw) for pw in passwords])
+def check_rule1(parse):
+    count = 0
+    for char in parse[3]:
+        count += char == parse[2]
+        if count > parse[1]:
+            return False
+    return count >= parse[0]
+
+def check_rule2(parse):
+    count = 0
+    first_match = parse[3][parse[0]-1] == parse[2]
+    second_match = parse[3][parse[1]-1] == parse[2]
+    return(first_match + second_match == 1)
+
+def check_passwords(passwords, rule_checker):
+    return sum([rule_checker(password_parse(pw)) for pw in passwords])
 
 def main(filename):
     passwords = get_passwords(filename)
-    print(check_passwords(passwords))
+    rule1 = check_passwords(passwords, check_rule1)
+    rule2 = check_passwords(passwords, check_rule2)
+    print(rule1, rule2)
 
 main(sys.argv[1])
